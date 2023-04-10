7a15925acd8358717ae4e08eb8c06eb384b10372
sbisht3 <sharadbisht3@gmail.com>
2023-04-10 14:56:35 +0530

    Second commit for the Game

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String args[]){
         TicTacToeGame game = new TicTacToeGame();
-        System.out.println("Winner :" + game.startGame());
+        System.out.println("Winner of the Game :" + game.startGame());
     }
 }
